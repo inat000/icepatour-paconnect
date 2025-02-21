7b6e2d985ce1d5f18dd2826e669a0de100e26c9f
inat000 <inattanon000@gmail.com>
2025-02-21 00:46:02 +0000

    run sandbox

--- app/layout.js
+++ app/layout.js
@@ -3,20 +3,14 @@ import '@/styles/globals.css'; // Updated path
 
 // 'use client'; // Required for client-side components
 
-import { Amplify } from 'aws-amplify';
-import '@aws-amplify/ui-react/styles.css'; // Amplify UI styles
-import { Authenticator } from '@aws-amplify/ui-react';
-import outputs from '../amplify_outputs.json';
-
-Amplify.configure(outputs);
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Authenticator>
+        
           {children}
-        </Authenticator>
+        
       </body>
     </html>
   );
